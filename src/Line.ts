dee8af2a2c24770098989ad1e6e2a9c6d0cf35fd
Mitchell Talyat <mitchell.talyat@gmail.com>
2022-10-13 18:33:23 -0500

    Line documentation

--- src/Line.ts
+++ src/Line.ts
@@ -1,12 +1,27 @@
+/**
+ * Represents a line of text from an assembly file.
+ */
 export class Line {
-    // the first thing in the string
+    /**
+     * The text on the line that appears before the first instance of a space.
+     */
     private _label: string;
 
-    // arguments in string form
+    /**
+     * The arguments of the line that appear after the label, until the end of the line.
+     */
     private _rawArgs: string;
 
+    /**
+     * The line number in the text document that this Line is from.
+     */
     private _lineNumber: number;
 
+    /**
+     * Creates a new Line from the given text and line number.
+     * @param text the raw text that this Line consists of.
+     * @param lineNumber the number of the line that this Line is from in the file.
+     */
     public constructor(text: string, lineNumber: number) {
         if (text) {
             // split incoming line by the first space
@@ -30,23 +45,42 @@ export class Line {
         this._lineNumber = lineNumber;
     }
 
-    // gets the label for this line
+    /**
+     * Gets the label for this Line.
+     * @returns the label/first word within this Line.
+     */
     public getLabel(): string {
         return this._label;
     }
 
+    /**
+     * Gets the args for this Line.
+     * @returns the args/text after the label within this Line.
+     */
     public getArgs(): string {
         return this._rawArgs;
     }
 
+    /**
+     * Gets the line number for this Line.
+     * @returns the line number.
+     */
     public getLineNumber(): number {
         return this._lineNumber;
     }
 
+    /**
+     * Checks if the label and args are both empty.
+     * @returns true of this Line is empty.
+     */
     public isEmpty(): boolean {
         return this._label.length === 0 && this._rawArgs.length === 0;
     }
 
+    /**
+     * Converts this Line into a readable string.
+     * @returns a readable string representation of this Line.
+     */
     public toString(): string {
         return `${this._lineNumber}\t${this._label} ${this._rawArgs}`;
     }
